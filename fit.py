cde2299d472e2423681a5c568372af8f0fbc6640
Wonseok Lee <57432949+wonseok-lee@users.noreply.github.com>
2022-03-06 20:36:49 +0900

    Update fit.py

--- fit.py
+++ fit.py
@@ -21,7 +21,7 @@ def objective_function(X, beta, y, lamda):
     '''
     return 1/2*np.linalg.norm(X.dot(beta)-y)**2+lamda*np.linalg.norm(beta,1)
 
-def coordinate(X, beta, y, lamda, iter_num):
+def coordinate(X, y, lamda, iter_num):
     '''
     coordinate descent for lasso regression (L2 penalized Regression)
 
@@ -32,8 +32,10 @@ def coordinate(X, beta, y, lamda, iter_num):
     :param iter_num: iteration number to infer beta
     :return: lists including time, objective value, MSE(loss)
     '''
-    # X = X / (np.linalg.norm(X))
+    X = (X - np.mean(X, 0)) / (np.std(X, 0))
+    X = X / (np.linalg.norm(X, axis = 0))
     _, k = X.shape
+    beta = np.zeros((k,1))
     record = Record()
     t = time.time()
     for _ in range(iter_num):
@@ -43,19 +45,20 @@ def coordinate(X, beta, y, lamda, iter_num):
             X_j = X[:, j].reshape(-1, 1)
             y_pred = X.dot(beta)
             beta_j = beta[j]
-            temp = np.matmul(X_j.T, (y - y_pred + beta_j * X_j))
-            beta[j] = soft_threshold(temp, lamda)
+            beta_j = np.matmul(X_j.T, (y - y_pred + beta_j * X_j))
+            beta[j] = soft_threshold(beta_j, lamda)
 
         obj_temp = objective_function(X, beta, y, lamda)
         loss_temp = np.linalg.norm(y - X.dot(beta)) ** 2
         record.add_obj(obj_temp)
         record.add_loss(loss_temp)
+        record.add_beta(beta)
 
     t = time.time() - t
     record.add_time(t)
     return record
 
-def proximal(X,beta,y,lamda,iter_num):
+def proximal(X,y,lamda,iter_num):
     '''
     proximal descent for lasso regression (L2 penalized Regression)
 
@@ -66,25 +69,27 @@ def proximal(X,beta,y,lamda,iter_num):
     :param iter_num: iteration number to infer beta
     :return: lists including time, objective value, MSE(loss)
     '''
+    X = (X - np.mean(X,0))/ (np.std(X,0))
+    X = X / (np.linalg.norm(X, axis=0))
+    beta = np.zeros((X.shape[1],1))
     c = np.linalg.norm(X) ** 2
     record = Record()
     t = time.time()
     for j in range(iter_num):
-        temp_beta = beta-lamda*np.matmul(X.T,(X.dot(beta)-y))
 
         # update beta
-        beta_new = soft_threshold(temp_beta/c,lamda/c)
+        beta = soft_threshold(beta - np.matmul(X.T, X.dot(beta) - y) / c, lamda / c)
 
-        obj_temp = objective_function(X,beta_new,y,lamda)
-        loss_temp = np.linalg.norm(y-X.dot(beta_new)) ** 2
+        obj_temp = objective_function(X,beta,y,lamda)
+        loss_temp = np.linalg.norm(y-X.dot(beta)) ** 2
         record.add_obj(obj_temp)
         record.add_loss(loss_temp)
-        beta = beta_new
+        record.add_beta(beta)
     t = time.time() - t
     record.add_time(t)
     return record
 
-def acc_proximal1(X,beta,y,lamda,iter_num):
+def acc_proximal1(X,y,lamda,iter_num):
     '''
     accelerated proximal descent for lasso regression (L2 penalized Regression)
     momentum = (j - 2) / (j + 1) * (beta_j - beta_{j-1})
@@ -96,39 +101,32 @@ def acc_proximal1(X,beta,y,lamda,iter_num):
     :param iter_num: iteration number to infer beta
     :return: lists including time, objective value, MSE(loss)
     '''
+    X = (X - np.mean(X, 0)) / (np.std(X, 0))
+    X = X / (np.linalg.norm(X, axis=0))
+    beta = np.zeros((X.shape[1], 1))
+    beta_old = beta.copy()
     c = np.linalg.norm(X) ** 2
     record = Record()
     t = time.time()
     for j in range(1,iter_num+1):
-        if j > 1:
-            # update momentum
-            v = beta + (j - 2) / (j + 1) * (beta - beta_old)
-            temp_beta = v - lamda * np.matmul(X.T, (X.dot(v) - y))
+        # update momentum
+        beta = beta + (j - 2) / (j + 1) * (beta - beta_old)
+        beta = beta - lamda * np.matmul(X.T, (X.dot(beta) - y))
 
-            # update beta
-            beta_new = soft_threshold(temp_beta / c, lamda / c)
+        # update beta
+        beta_new = soft_threshold(beta / c, lamda / c)
 
-            obj_temp = objective_function(X, beta_new, y, lamda)
-            loss_temp = np.linalg.norm(y - X.dot(beta_new)) ** 2
-            record.add_obj(obj_temp)
-            record.add_loss(loss_temp)
-            beta, beta_old = beta_new, beta
-        else:
-            temp_beta = beta - lamda * np.matmul(X.T, (X.dot(beta) - y))
-
-            # update beta
-            beta = soft_threshold(temp_beta / c, lamda / c)
-
-            obj_temp = objective_function(X, beta, y, lamda)
-            loss_temp = np.linalg.norm(y - X.dot(beta)) ** 2
-            record.add_obj(obj_temp)
-            record.add_loss(loss_temp)
-            beta_old = beta
+        obj_temp = objective_function(X, beta_new, y, lamda)
+        loss_temp = np.linalg.norm(y - X.dot(beta_new)) ** 2
+        record.add_obj(obj_temp)
+        record.add_loss(loss_temp)
+        record.add_beta(beta)
+        beta, beta_old = beta_new, beta
     t = time.time() - t
     record.add_time(t)
     return record
 
-def acc_proximal2(X,beta,y,lamda,iter_num):
+def acc_proximal2(X,y,lamda,iter_num):
     '''
     accelerated proximal descent for lasso regression (L2 penalized Regression)
     momentum = sqrt((1 + 4*(j**2)))/2
@@ -140,27 +138,32 @@ def acc_proximal2(X,beta,y,lamda,iter_num):
     :param iter_num: iteration number to infer beta
     :return: lists including time, objective value, MSE(loss)
     '''
+    X = (X - np.mean(X, 0)) / (np.std(X, 0))
+    X = X / (np.linalg.norm(X, axis=0))
+    beta = np.zeros((X.shape[1], 1))
     c = np.linalg.norm(X) ** 2
     k = 1
-    v = beta
     record = Record()
+    z = beta.copy()
     t = time.time()
     for j in range(1,iter_num+1):
-        beta_old = beta
-        temp_beta = v - lamda * np.matmul(X.T, (X.dot(v) - y))
+        beta_old = beta.copy()
 
         # update beta
-        beta = soft_threshold(temp_beta / c, lamda / c)
+        z = z + np.matmul(X.T, (y - np.matmul(X, z))) / c
+        beta = soft_threshold(z, lamda / c)
 
         # update momentum
         k0 = k
-        k = (1 + np.sqrt(1 + 4*(k**2)))/2
-        v = beta + ((k0 - 1)/k) * (beta - beta_old)
+        k = (1 + np.sqrt(1 + 4 * k ** 2)) / 2
+        z = beta + ((k0 - 1) / k) * (beta - beta_old)
 
         obj_temp = objective_function(X, beta, y, lamda)
         loss_temp = np.linalg.norm(y - X.dot(beta)) ** 2
         record.add_obj(obj_temp)
         record.add_loss(loss_temp)
+        record.add_beta(beta)
+
     t = time.time() - t
     record.add_time(t)
     return record
@@ -175,9 +178,11 @@ def admm(X,y,lamda,rho,iter_num):
     :param iter_num: iteration number to infer beta
     :return: lists including time, objective value, MSE(loss)
     '''
+    X = (X - np.mean(X, 0)) / (np.std(X, 0))
+    X = X / (np.linalg.norm(X, axis=0))
     n, k = X.shape
-    alpha = np.ones((k, 1))
-    omega = np.ones((k, 1))
+    alpha = np.zeros((k, 1))
+    omega = np.zeros((k, 1))
     c = np.linalg.norm(X) ** 2
     t = time.time()
     record = Record()
@@ -192,6 +197,7 @@ def admm(X,y,lamda,rho,iter_num):
         loss_temp = np.linalg.norm(y - X.dot(beta_new)) ** 2
         record.add_obj(obj_temp)
         record.add_loss(loss_temp)
+        record.add_beta(beta)
 
         beta, alpha, omega = beta_new, alpha_new, omega_new
     t = time.time() - t
